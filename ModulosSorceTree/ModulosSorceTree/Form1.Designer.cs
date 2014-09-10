8f0b68fb15d9ae32057b2a521c3e2bd874baeb9b
Eleord <carlos93r@live.com.mx>
2014-09-10 09:44:09 -0600

    Nuevo Mensaje al dar respuesta

--- ModulosSorceTree/ModulosSorceTree/Form1.Designer.cs
+++ ModulosSorceTree/ModulosSorceTree/Form1.Designer.cs
@@ -43,9 +43,11 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(0, 0);
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.Size = new System.Drawing.Size(70, 20);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Sumar";
@@ -55,9 +57,11 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(0, 23);
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.radioButton2.Location = new System.Drawing.Point(12, 35);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.Size = new System.Drawing.Size(72, 20);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Restar";
@@ -67,9 +71,11 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(0, 42);
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.radioButton3.Location = new System.Drawing.Point(12, 54);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(54, 17);
+            this.radioButton3.Size = new System.Drawing.Size(71, 20);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Dividir";
@@ -79,9 +85,11 @@
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(0, 65);
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.radioButton4.Location = new System.Drawing.Point(12, 77);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(72, 17);
+            this.radioButton4.Size = new System.Drawing.Size(97, 20);
             this.radioButton4.TabIndex = 3;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "Multiplicar";
@@ -91,9 +99,11 @@
             // radioButton5
             // 
             this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(0, 88);
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.radioButton5.Location = new System.Drawing.Point(12, 100);
             this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(84, 17);
+            this.radioButton5.Size = new System.Drawing.Size(112, 20);
             this.radioButton5.TabIndex = 4;
             this.radioButton5.TabStop = true;
             this.radioButton5.Text = "Exponenciar";
@@ -102,23 +112,25 @@
             // 
             // nudPrimero
             // 
-            this.nudPrimero.Location = new System.Drawing.Point(12, 124);
+            this.nudPrimero.Location = new System.Drawing.Point(162, 12);
             this.nudPrimero.Name = "nudPrimero";
             this.nudPrimero.Size = new System.Drawing.Size(120, 20);
             this.nudPrimero.TabIndex = 5;
             // 
             // nupSegundo
             // 
-            this.nupSegundo.Location = new System.Drawing.Point(164, 124);
+            this.nupSegundo.Location = new System.Drawing.Point(162, 55);
             this.nupSegundo.Name = "nupSegundo";
             this.nupSegundo.Size = new System.Drawing.Size(120, 20);
             this.nupSegundo.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(102, 172);
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(162, 87);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 43);
+            this.button1.Size = new System.Drawing.Size(120, 39);
             this.button1.TabIndex = 7;
             this.button1.Text = "Accion, me da igual cual";
             this.button1.UseVisualStyleBackColor = true;
@@ -128,7 +140,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(310, 160);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.nupSegundo);
             this.Controls.Add(this.nudPrimero);
@@ -137,6 +150,7 @@
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Probando el trabajo";
             ((System.ComponentModel.ISupportInitialize)(this.nudPrimero)).EndInit();
