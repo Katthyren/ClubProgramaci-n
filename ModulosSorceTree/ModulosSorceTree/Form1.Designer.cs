0bbc1bf25e11c1a4f7ba6bbe1505225379d45505
Eleord <carlos93r@live.com.mx>
2014-10-14 10:28:17 -0600

    no message

--- ModulosSorceTree/ModulosSorceTree/Form1.Designer.cs
+++ ModulosSorceTree/ModulosSorceTree/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.nudPrimero)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nupSegundo)).BeginInit();
             this.SuspendLayout();
@@ -141,6 +142,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 8;
+            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(17, 175);
@@ -151,14 +153,21 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(307, 162);
-            this.Controls.Add(this.label1);
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.nupSegundo);
@@ -194,6 +203,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
